d83e173d2dbd01fe4f1e4dd562bd96b659f630fb
Jonatan Quiroz <yonnyce@gmail.com>
2020-07-26 19:33:40 -0500

    Commit inicial

---
+++ src/test/java/com/ucentral/microservicio/MicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucentral.microservicio;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
