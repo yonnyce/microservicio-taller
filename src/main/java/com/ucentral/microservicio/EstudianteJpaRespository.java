0c95e574b788c8eba44303daa39035e64e6d0d45
Jonatan Quiroz <yonnyce@gmail.com>
2020-07-27 20:30:46 -0500

    Crear repositorio de persistencia

---
+++ src/main/java/com/ucentral/microservicio/EstudianteJpaRespository.java
@@ -0,0 +1,7 @@
+package com.ucentral.microservicio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstudianteJpaRespository extends JpaRepository<EstudianteEntity, Integer> {
+
+}
